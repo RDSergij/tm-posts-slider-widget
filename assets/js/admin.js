2bb785988d18f1f54b2e5372405cb322e3c43d6a
RDSergij <sergijlys@gmail.com>
2015-12-30 12:43:40 +0200

    Style guide

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,15 +1,20 @@
-jQuery( document ).on( 'widget-updated', function( e, widget ) {
-	CHERRY_API.ui_elements.switcher.init( jQuery( 'body' ) );
-});
-
-jQuery( document ).on( 'widget-updated ready', function( e, widget ) {
-	jQuery( 'div#button-show' ).click( function() {
-		var _this = jQuery( this );
-		var button_is = _this.find( 'input#' + window.TMWidgetParam.button_is ).val();
-		if ( 'false' === button_is ) {
-			_this.find( 'p.tm-post-slider-button-text' ).hide();
-		} else {
-			_this.find( 'p.tm-post-slider-button-text' ).show();
-		}
+jQuery( document ).ready( function() {
+	jQuery( document ).on( 'widget-updated', function() {
+		window.CHERRY_API.ui_elements.switcher.init( jQuery( 'body' ) );
 	});
-});
+
+	jQuery( document ).on( 'widget-updated ready', function() {
+		jQuery( 'div#button-show' ).click( function() {
+			var _this = jQuery( this );
+			var Button_is = _this.find( 'input#' + window.TMWidgetParam.button_is ).val();
+			if ( 'false' === Button_is ) {
+				_this.find( 'p.tm-post-slider-button-text' ).hide();
+			} else {
+				_this.find( 'p.tm-post-slider-button-text' ).show();
+			}
+		}
+		);
+	}
+	);
+}
+		);
--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -1,9 +1,11 @@
-var swiper = new Swiper( '.swiper-container', {
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        slidesPerView: 1,
-        paginationClickable: true,
-        spaceBetween: 30,
-        loop: true
-    } );
+jQuery( document ).ready( function() {
+	window.swiper = new window.Swiper( '.swiper-container', {
+		pagination: '.swiper-pagination',
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev',
+		slidesPerView: 1,
+		paginationClickable: true,
+		spaceBetween: 30,
+		loop: true
+	} );
+});
