571145101a8dd52b79c6d65381a6e875517c7b8c
RDSergij <sergijlys@gmail.com>
2016-01-05 11:13:41 +0200

    Code style

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,24 +1,24 @@
 /**
  * Events list
  */
-jQuery( document ).ready( init_widget );
-jQuery( document ).on( 'widget-updated widget-added ready', init_widget );
+jQuery( document ).ready( initWidget );
+jQuery( document ).on( 'widget-updated widget-added ready', initWidget );
 
 /**
  * Initialization widget js
- * 
+ *
  * @returns {undefined}
  */
-function init_widget() {
+function initWidget() {
 	window.CHERRY_API.ui_elements.switcher.init( jQuery( 'body' ) );
-	
+
 	jQuery( '.tm-post-slider-form-widget .cherry-switcher-wrap' ).click(
 		function() {
 			var _this = jQuery( this );
 			_this.find( 'input[type=hidden]' ).trigger( 'change' );
 		}
 	);
-	
+
 	jQuery( '.tm-post-slider-form-widget div#button-show' ).click( function() {
 		var _this = jQuery( this );
 		console.log( _this.find( 'input[type=hidden]' ).val() );
