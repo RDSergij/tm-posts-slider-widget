e108b8450b4d5067e9b9865caddb394d0cd6ac9d
RDSergij <sergijlys@gmail.com>
2016-01-19 11:23:54 +0200

    Fix js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -10,21 +10,13 @@ jQuery( document ).on( 'widget-updated widget-added ready', initWidgetTMPostsSli
  * @returns {undefined}
  */
 function initWidgetTMPostsSlider() {
-
-//	jQuery( '.tm-post-slider-form-widget .cherry-switcher-wrap' ).click(
-//		function() {
-//			var _this = jQuery( this );
-//			_this.find( 'input[type=hidden]' ).trigger( 'change' );
-//		}
-//	);
-
-//	jQuery( '.tm-post-slider-form-widget div#button-show' ).click( function() {
-//		var _this = jQuery( this );
-//		if ( 'false' === _this.find( 'input[type=hidden]' ).val() ) {
-//			_this.find( 'p.tm-post-slider-button-text' ).hide();
-//		} else {
-//			_this.find( 'p.tm-post-slider-button-text' ).show();
-//		}
-//	}
-//	);
+	jQuery( '.tm-post-slider-form-widget div#button-show' ).click( function() {
+		var _this = jQuery( this );
+		if ( 'false' === _this.find( 'input[type=radio]:checked' ).val() ) {
+			_this.find( 'p.tm-post-slider-button-text' ).hide();
+		} else {
+			_this.find( 'p.tm-post-slider-button-text' ).show();
+		}
+	}
+	);
 }
